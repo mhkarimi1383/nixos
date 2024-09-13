adb2edda9ae8b5806f4d9e3db42fe0b9c937b5e5
Muhammed Hussein Karimi <info@karimi.dev>
2024-09-13 18:17:37 +0330

    init: first commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "NixOS Flake config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
