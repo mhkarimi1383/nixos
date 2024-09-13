91af1d2cb4670f41d9c583fc6a60d7c573948033
Muhammed Hussein Karimi <info@karimi.dev>
2024-09-13 18:29:32 +0330

    feat: hypr added

--- home.nix
+++ home.nix
@@ -45,6 +45,10 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".config/hypr" = {
+      source = ./hypr;
+      recursive = true;
+    };
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
