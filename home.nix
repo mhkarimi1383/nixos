b9d59da56a480ce4448c26b9aa1d3e6120962540
Muhammed Hussein Karimi <info@karimi.dev>
2024-09-13 18:50:30 +0330

    add kitty

--- home.nix
+++ home.nix
@@ -49,6 +49,10 @@
       source = ./hypr;
       recursive = true;
     };
+    ".config/kitty" = {
+      source = ./kitty;
+      recursive = true;
+    };
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
