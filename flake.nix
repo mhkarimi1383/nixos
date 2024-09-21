b40dca4f7536d5ae23aab5d62e6b89eb73178032
Muhammed Hussein Karimi <info@karimi.dev>
2024-09-22 00:29:10 +0330

    :heavy_plus_sign: Wayland package repo
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-
+    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
