d1d6cf090aa06bd9ee4458ad6280ceae8e3d19c8
Muhammed Hussein Karimi <info@karimi.dev>
2024-12-02 20:10:57 +0330

    :zap: Increase battery charge threshold (Thanks to random energy losts)
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- configuration.nix
+++ configuration.nix
@@ -232,7 +232,7 @@
         serviceConfig = {
           Type = "oneshot";
           Restart = "on-failure";
-          ExecStart = "/run/current-system/sw/bin/bash -c 'echo 60 > /sys/class/power_supply/BAT0/charge_control_end_threshold'";
+          ExecStart = "/run/current-system/sw/bin/bash -c 'echo 80 > /sys/class/power_supply/BAT0/charge_control_end_threshold'";
         };
         wantedBy = ["graphical-session.target" "default.target"];
       };
