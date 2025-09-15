869c36194611539496f05529991e9e5d39dcd4a8
Muhammed Hussein Karimi <info@karimi.dev>
2025-09-15 15:03:16 +0330

    :lipstick: Prefer dark mode in GTK
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- home.nix
+++ home.nix
@@ -7,6 +7,9 @@
   nixpkgs.config.allowUnfree = true;
   dconf = {
     settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
       "org/virt-manager/virt-manager/connections" = {
         autoconnect = ["qemu:///system"];
         uris = ["qemu:///system"];
