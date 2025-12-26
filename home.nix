c30f6486f002df36a76423e8c9370c65685908c2
Muhammed Hussain Karimi <info@karimi.dev>
2025-12-26 10:32:01 +0330

    :heavy_plus_sign: Wlogout config
    Signed-off-by: Muhammed Hussain Karimi <info@karimi.dev>

--- home.nix
+++ home.nix
@@ -496,6 +496,9 @@
     ".config/cava/config" = {
       source = ./cava.ini;
     };
+    ".config/wlogout" = {
+      source = ./wlogout;
+    };
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
